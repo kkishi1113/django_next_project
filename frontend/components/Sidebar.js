7cdb20b5c1840f647794a9d22e60cb9ee613df4a
kkishi1113 <w.destroy1113@gmail.com>
2024-07-04 01:50:12 +0900

    サイドバーにメニュー追加

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Sidebar = ({ isVisible, toggleSidebar }) => {
+const Sidebar = ({ isVisible, toggleSidebar, apps, handleSelectApp }) => {
+
+  const menuList = [
+    { name: 'Dashboard', content: <Link href="/dashboard"><span className="block py-2 px-4 text-sm hover:bg-gray-700">ダッシュボード</span></Link>},
+    { name: 'Profile', content: <Link href="/profile"><span className="block py-2 px-4 text-sm hover:bg-gray-700">プロフィール</span></Link>},
+    { name: 'Settings', content: <Link href="/settings"><span className="block py-2 px-4 text-sm hover:bg-gray-700">設定</span></Link>},
+    ...apps,
+  ]
+
   return (
     <div className={`fixed inset-y-0 left-0 transform ${isVisible ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out w-64 bg-gray-800 text-white shadow-md`}>
       <button onClick={toggleSidebar} className={`absolute top-2 right-0 transform ${isVisible ?  '' : 'translate-x-full'} p-2 bg-gray-800 text-white shadow-md rounded-r-md`}>
@@ -11,27 +19,13 @@ const Sidebar = ({ isVisible, toggleSidebar }) => {
         <h2 className="text-2xl font-bold">サイドバー</h2>
       </div>
       <ul className="relative">
-        <li className="relative">
-          <Link href="/dashboard">
-            <span className="block py-2 px-4 text-sm hover:bg-gray-700">
-              ダッシュボード
-            </span>
-          </Link>
-        </li>
-        <li className="relative">
-          <Link href="/profile">
-            <span className="block py-2 px-4 text-sm hover:bg-gray-700">
-              プロフィール
-            </span>
-          </Link>
-        </li>
-        <li className="relative">
-          <Link href="/settings">
-            <span className="block py-2 px-4 text-sm hover:bg-gray-700">
-              設定
-            </span>
-          </Link>
-        </li>
+        { menuList.map((menu, index) => (
+          <li key={index} className="relative">
+              <Link href="#" onClick={()=>handleSelectApp(menu)} className="block py-2 px-4 text-sm hover:bg-gray-700" >
+                {menu.name}
+              </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -3,10 +3,14 @@ import { useRouter } from 'next/router';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import Todo from '../components/Todo';
+import Link from 'next/link';
 import { fetchData, postData, deleteData, patchData } from '../utils/utils';
 
 const apps = [
   { name: 'Todo', content: <Todo /> },
+  { name: 'Link', content: <Link href="/dashboard"><span className="text-2xl font-bold">Dashboard</span></Link> },
+  { name: 'Button0', content: <div><button  className="bg-green-500 text-white px-4 py-2 rounded" value="0" onClick={(e) => console.log(e.target.value)}>ボタン0</button></div>},
+  { name: 'Button1', content: <div><button className="bg-blue-500 text-white px-4 py-2 rounded" value="1" onClick={(e) => console.log(e.target.value)}>ボタン1</button></div> },
 ]
 
 const Dashboard = () => {
@@ -45,13 +49,17 @@ const Dashboard = () => {
 
   return (
     <div className="flex">
-      <Sidebar isVisible={isSidebarVisible} toggleSidebar={() => setSidebarVisible(!isSidebarVisible)} />
+      <Sidebar 
+        isVisible={isSidebarVisible} 
+        toggleSidebar={() => setSidebarVisible(!isSidebarVisible)} 
+        apps={apps}
+        handleSelectApp={handleSelectApp}
+      />
       <div className="flex-grow container mx-auto ml-72 mr-8">
         <Header username={username} postData={postData} />
         <main>
           <h2 className="text-lg mb-4">ようこそ、<span className="font-bold">{username}</span>さん</h2>
           {mainContent.content}
-          {/* <Todo fetchData={fetchData} postData={postData} patchData={patchData} deleteData={deleteData} /> */}
         </main>
       </div>
     </div>
