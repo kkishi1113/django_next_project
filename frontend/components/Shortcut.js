1e44c51565bfeeddf498a68279bc72c63675f19a
kkishi1113 <w.destroy1113@gmail.com>
2024-08-15 15:34:25 +0900

    shadcn/uiからHoverCardをインストール。ショートカットホバー時にタイトルの全文が表示されるように修正。編集ボタンホバー時にショートカット全体が透明にならないように修正。

--- frontend/components/Shortcut.js
+++ frontend/components/Shortcut.js
@@ -41,6 +41,13 @@ import {
 } from "@/components/ui/card"
 import Link from 'next/link';
 
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+
 const formSchema = z.object({
   id: z.number().optional(),
   title: z.string().min(1,{message: "入力が必須です。"}).max(100,{message: "最大文字数は100文字です。"}),
@@ -171,70 +178,79 @@ const Shortcut = () => {
         </Dialog>
         <div className="grid grid-cols-2 xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-4 gap-4 py-4">
           {shortcuts.map((shortcut)=>(
-            <Card key={shortcut.id} className="border-white shadow-white h-full w-full relative hover:opacity-50 group">
-              <Dialog>
-                <DialogTrigger asChild className="rounded-full">
-                  <Button variant="ghost" className="aspect-square absolute top-0 right-0 z-20 hidden group-hover:block" onClick={() => handleEditClick(shortcut)}>
-                    <FontAwesomeIcon icon={faEllipsisVertical} className="" />
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <Form {...form}>
-                    <form onSubmit={form.handleSubmit(handleUpdateShortcut)} className="space-y-8">
-                      <DialogHeader>
-                        <DialogTitle>ショートカットを編集</DialogTitle>
-                        <DialogDescription></DialogDescription>
-                      </DialogHeader>
-                      <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>表示名</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Google" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name="url"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>URL</FormLabel>
-                            <FormControl>
-                              <Input placeholder="https://www.google.com/" {...field} />
-                            </FormControl>
-                            <FormDescription></FormDescription>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <DialogFooter>
-                        <DialogClose asChild>
-                          <Button variant="destructive" type="button" onClick={() => handleDeleteShortcut(shortcut.id)}>削除</Button>
-                        </DialogClose>
-                        <DialogClose asChild>
-                          <Button type="submit" disabled={!form.formState.isValid}>保存</Button>
-                        </DialogClose>
-                      </DialogFooter>
-                    </form>
-                  </Form>
-                </DialogContent>
-              </Dialog>
-              <Link href={shortcut.url} target="_blank" rel="noopener noreferrer" className="block aspect-square">
-                <CardContent className="absolute inset-0 flex place-items-center ">
-                  {/* <img src={shortcut.url+"/favicon.ico"} className="object-contain w-10" /> */}
-                  {/* <img src={"http://www.google.com/s2/favicons?domain="+shortcut.url}  className="object-contain w-10" /> */}
-                  <img src={"https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url="+shortcut.url+"&size=128"}  className="m-auto w-12" />
-                </CardContent>
-                <CardFooter className="absolute bottom-0 w-full flex justify-center h-1/3 px-1">
-                  <p className="text-center overflow-hidden whitespace-nowrap text-ellipsis">{shortcut.title}</p>
-                </CardFooter>
-              </Link>
-            </Card>
+            <HoverCard>
+              <Card key={shortcut.id} className="border-white shadow-white h-full w-full relative group">
+                <Dialog>
+                  <DialogTrigger asChild className="rounded-full">
+                    <Button variant="ghost" className="aspect-square absolute top-0 right-0 z-20 hidden group-hover:block" onClick={() => handleEditClick(shortcut)}>
+                      <FontAwesomeIcon icon={faEllipsisVertical} className="" />
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <Form {...form}>
+                      <form onSubmit={form.handleSubmit(handleUpdateShortcut)} className="space-y-8">
+                        <DialogHeader>
+                          <DialogTitle>ショートカットを編集</DialogTitle>
+                          <DialogDescription></DialogDescription>
+                        </DialogHeader>
+                        <FormField
+                          control={form.control}
+                          name="title"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>表示名</FormLabel>
+                              <FormControl>
+                                <Input placeholder="Google" {...field} />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <FormField
+                          control={form.control}
+                          name="url"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>URL</FormLabel>
+                              <FormControl>
+                                <Input placeholder="https://www.google.com/" {...field} />
+                              </FormControl>
+                              <FormDescription></FormDescription>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                        <DialogFooter>
+                          <DialogClose asChild>
+                            <Button variant="destructive" type="button" onClick={() => handleDeleteShortcut(shortcut.id)}>削除</Button>
+                          </DialogClose>
+                          <DialogClose asChild>
+                            <Button type="submit" disabled={!form.formState.isValid}>保存</Button>
+                          </DialogClose>
+                        </DialogFooter>
+                      </form>
+                    </Form>
+                  </DialogContent>
+                </Dialog>
+                <HoverCardTrigger>
+                  <Link href={shortcut.url} target="_blank" rel="noopener noreferrer" className="block aspect-square hover:opacity-50">
+                    <CardContent className="absolute inset-0 flex place-items-center ">
+                      {/* <img src={shortcut.url+"/favicon.ico"} className="object-contain w-10" /> */}
+                      {/* <img src={"http://www.google.com/s2/favicons?domain="+shortcut.url}  className="object-contain w-10" /> */}
+                      <img src={"https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url="+shortcut.url+"&size=128"}  className="m-auto w-12" />
+                    </CardContent>
+                    <CardFooter className="absolute bottom-0 w-full flex justify-center h-1/3 px-1">
+                        <p className="text-center overflow-hidden whitespace-nowrap text-ellipsis">{shortcut.title}</p>
+                    </CardFooter>
+                  </Link>
+                </HoverCardTrigger>
+              </Card>
+              <HoverCardContent className="text-center text-sm w-auto max-w-96">
+                {shortcut.title}
+              </HoverCardContent>
+            </HoverCard>
+            
+            
           ))}
         </div> 
       </>
